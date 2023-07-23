c4e4e38339c22a5c74935c47622cff7bc6ec1a01
malla-lokesh <121810307046@gitam.in>
2023-07-23 19:05:53 +0530

    Introduction to Middlewares

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.send('first middleware\n');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/msg', (req, res, next) => {
+    res.send(`<h1>message route</h1>\n`);
     next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+        <form action="/product" method="POST">
+            <input type="text" name="product"/>
+            <input type="number" name="size"/>
+            <button type="submit">send</button>
+        </form>
+    `);
 })
 
-app.use((req, res, next) => {
-    console.log('second middleware');
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/msg');
 })
 
 console.log('from express js');
