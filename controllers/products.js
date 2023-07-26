61620559dc1e779390580ba77748718edfe842a2
malla-lokesh <121810307046@gitam.in>
2023-07-26 15:16:45 +0530

    Models task

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -11,19 +11,21 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.laptop);
+    product.save();
     res.redirect('/');
-    console.log(req.body);
-    products.push({title: req.body.laptop});
 }
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    const products = Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
     // res.sendFile(path.join(rootDir, './', 'views', 'shop.html'));
 }
